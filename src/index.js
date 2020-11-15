e0753b39c609e6551a038dba3291875582942b56
Andrey0295 <andreevicandrej00@gmail.com>
2020-11-15 18:11:07 +0200

    Производит стилизацию первых трех секций

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./sass/main.scss";
+import "./js/button_events.js"
+
+
+const menuEl= documentQuerySelector('.site-nav');
+const openBtnEl= documentQuerySelector('.open-button');
+const closeBtnEl= documentQuerySelector('.close-button');
+
+
+console.log('Hello');
+
+
+
