864b22a2294766ae25b4ec3473ae2a3f8db89189
Andrey0295 <andreevicandrej00@gmail.com>
2020-11-15 20:46:51 +0200

    Делает открытие меню в моб.версии

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import "./sass/main.scss";
 import "./js/button_events.js"
 
 
-const menuEl= documentQuerySelector('.site-nav');
-const openBtnEl= documentQuerySelector('.open-button');
-const closeBtnEl= documentQuerySelector('.close-button');
 
-
-console.log('Hello');
 
 
 
--- src/js/button_events.js
+++ src/js/button_events.js
@@ -1,1 +1,66 @@
+
+const refs={
+    menuEl: document.querySelector('.site-nav'),
+    openBtnEl: document.querySelector('.open-button'),
+    closeBtnEl: document.querySelector('.menu-close-btn')
+}
+
+
+refs.menuEl.classList.add('is-hidden')
+// refs.closeBtnEl.classList.add('is-hidden')
+
+refs.openBtnEl.addEventListener('click', onClickBtnOpen)
+refs.closeBtnEl.addEventListener('click', onClickBtnClose)
+// refs.openBtnEl.addEventListener('click', closeMenuHeandler)
+
+// function closeMenuHeandler(e){
+//    if(refs.menuEl.classList.contains('is-open')){
+//     // refs.openBtnEl.classList.remove('is-hidden')
+//     // refs.closeBtnEl.classList.replace('is-open', 'is-hidden')
+//     // refs.menuEl.classList.replace('is-open', 'is-hidden')
+
+//    }
+   
+
+// }
+
+function onClickBtnOpen(evt){
+    // refs.openBtnEl.classList.add('is-hidden')
+    
+    refs.openBtnEl.classList.toggle('is-hidden')
+    // refs.closeBtnEl.classList.toggle( 'is-hidden')
+    refs.menuEl.classList.toggle('is-hidden' )
+
+
+
+    console.log(evt);
+
+    // if(refs.menuEl.classList.contains('is-open')){
+    //     refs.openBtnEl.classList.toggle('is-hidden')
+    //     refs.closeBtnEl.classList.add( 'is-hidden')
+    //     refs.menuEl.classList.replace('is-open' ,'is-hidden')
+    // }
+
+    // if(refs.openBtnEl.classList.add('is-hidden')){
+    //     closeMenuHeandler()
+    // }
+   
+} 
+function onClickBtnClose(e){
+    refs.openBtnEl.classList.toggle('is-hidden')
+    // refs.closeBtnEl.classList.toggle( 'is-hidden')
+    refs.menuEl.classList.toggle('is-hidden' )
+}
+
+
+
+// function onClickBtnClose(evt){
+//     refs.openBtnEl.classList.remove('is-hidden')
+//     refs.closeBtnEl.classList.toggle('is-hidden')
+//     refs.menuEl.classList.toggle('is-hidden')
+
+// }
+
+
+
 console.log('Hello');
